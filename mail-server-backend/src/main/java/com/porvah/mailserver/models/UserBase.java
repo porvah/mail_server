610c90fc8fbe067aa9570d08eb497b962f318b90
ayman Algamal <ayman01287985950@gmail.com>
2023-12-14 20:03:44 +0200

    Making the UserBase a Singleton Class.

--- mail-server-backend/src/main/java/com/porvah/mailserver/models/UserBase.java
+++ mail-server-backend/src/main/java/com/porvah/mailserver/models/UserBase.java
@@ -4,41 +4,52 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class UserBase {
+
+    //Singleton
+    private static UserBase instance;
     private List<User> users;
     private List<User> loggedUsers;
 
-    public UserBase(){
-        this.users = new ArrayList<User>();
-        this.loggedUsers = new ArrayList<User>();
+    private UserBase() {
+        this.users = new ArrayList<>();
+        this.loggedUsers = new ArrayList<>();
     }
-    public void addUser(User user){
+
+    public static UserBase getInstance() {
+        if (instance == null) {
+            instance = new UserBase();
+        }
+        return instance;
+    }
+
+    public void addUser(User user) {
         this.users.add(user);
     }
-    public void deleteUser(String email){
-        for(int i = 0; i < this.users.size(); i++) {
+
+    public void deleteUser(String email) {
+        for (int i = 0; i < this.users.size(); i++) {
             if (this.users.get(i).getEmail().equals(email))
                 this.users.remove(i);
         }
     }
-    public User getUser(String email){
-        for(int i = 0; i < this.users.size(); i++){
-            if(this.users.get(i).getEmail().equals(email)){
+
+    public User getUser(String email) {
+        for (int i = 0; i < this.users.size(); i++) {
+            if (this.users.get(i).getEmail().equals(email)) {
                 return this.users.get(i);
             }
         }
-        throw new RuntimeException();
+        throw new RuntimeException("Couldn't find user with email: " + email + " in the user base.");
     }
 
-    public int logUser(String email){
+    public int logUser(String email) {
         User logged = this.getUser(email);
         int id = this.loggedUsers.size();
         this.loggedUsers.add(logged);
         return id;
     }
 
-    public User getLoggedUser(int id){
+    public User getLoggedUser(int id) {
         return this.loggedUsers.get(id);
     }
-
-
 }
