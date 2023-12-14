06989667128f22101c54b972c06daacd5b6fb809
ayman Algamal <ayman01287985950@gmail.com>
2023-12-14 20:06:16 +0200

    Creating the sign up method in the cotroller.

--- mail-server-backend/src/main/java/com/porvah/mailserver/controllers/MailController.java
+++ mail-server-backend/src/main/java/com/porvah/mailserver/controllers/MailController.java
@@ -1,12 +1,27 @@
 package com.porvah.mailserver.controllers;
 
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.porvah.mailserver.models.User;
+import com.porvah.mailserver.models.UserBase;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Map;
 
 @RestController
 @RequestMapping("/")
 @CrossOrigin()
 public class MailController {
+
+    @PostMapping("/signup")
+    public Boolean signup(@RequestBody Map<String, Object> body){
+
+        String email = (String) body.get("email");
+        String password = (String) body.get("password");
+
+        User user = new User(email, password);
+        UserBase.getInstance().addUser(user);
+
+        return true;
+    }
+
 
 }
