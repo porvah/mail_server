3c7b267e4e730fb680d226801a101b30f11e0c44
ayman Algamal <ayman01287985950@gmail.com>
2023-12-15 02:53:28 +0200

    creating userData Class

--- mail-server-backend/src/main/java/com/porvah/mailserver/models/UserData.java
+++ mail-server-backend/src/main/java/com/porvah/mailserver/models/UserData.java
@@ -1,7 +1,53 @@
 package com.porvah.mailserver.models;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class UserData {
 
-    // Singleton
+    private MailFolder inbox;
+    private MailFolder sent;
+    private MailFolder trash;
+    private MailFolder draft;
+
+    private Map<String, MailFolder> folders;
+
+    public UserData() {
+        this.inbox = new MailFolder();
+        this.sent = new MailFolder();
+        this.trash = new MailFolder();
+        this.draft = new MailFolder();
+
+        this.folders = new HashMap<>();
+    }
+
+
+    public MailFolder getInbox() {
+        return inbox;
+    }
+
+    public MailFolder getSent() {
+        return sent;
+    }
+
+    public MailFolder getTrash() {
+        return trash;
+    }
+
+    public MailFolder getDraft() {
+        return draft;
+    }
+
+    public MailFolder getCustomFolder(String folderName){
+        return folders.get(folderName);
+    }
+
+    public void addCustomFolder(String folderName){
+        folders.put(folderName, new MailFolder());
+    }
+
+    public void removeCustomFolder(String folderName){
+        folders.remove(folderName);
+    }
 
 }
