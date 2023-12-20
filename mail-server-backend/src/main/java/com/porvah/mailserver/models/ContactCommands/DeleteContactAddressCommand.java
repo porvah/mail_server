6bd114ae28283b6b99890477cc12853b80aa4d86
ayman Algamal <ayman01287985950@gmail.com>
2023-12-20 17:40:41 +0200

    Adding DeleteContactAddressCommand class.

--- mail-server-backend/src/main/java/com/porvah/mailserver/models/ContactCommands/DeleteContactAddressCommand.java
+++ mail-server-backend/src/main/java/com/porvah/mailserver/models/ContactCommands/DeleteContactAddressCommand.java
@@ -4,19 +4,25 @@ import com.porvah.mailserver.models.Contact;
 
 import java.util.Map;
 
-public class DeleteContactCommand implements ContactCommandIF {
+public class DeleteContactAddressCommand implements ContactCommandIF{
 
     final private String name;
+    final private String email;
 
-    public DeleteContactCommand(String name) {
+    public DeleteContactAddressCommand(String name, String email) {
         this.name = name;
+        this.email = email;
+
     }
     @Override
     public boolean execute(Map<String, Contact> contacts) {
+
         if(contacts.containsKey(name)){
-            contacts.remove(name);
+            Contact contact = contacts.get(name);
+            contact.removeEmail(email);
             return true;
         }
         return false;
     }
+
 }
