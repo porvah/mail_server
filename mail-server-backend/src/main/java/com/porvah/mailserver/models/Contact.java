018d64b70f7c0c75f61e16ef86228a60c74a2b0d
porvah <3msabdullah@gmail.com>
2023-12-22 06:07:21 +0200

    sorting problem solved

--- mail-server-backend/src/main/java/com/porvah/mailserver/models/Contact.java
+++ mail-server-backend/src/main/java/com/porvah/mailserver/models/Contact.java
@@ -13,35 +13,21 @@ public class Contact {
         this.name = name;
         this.emails = new ArrayList<>();
         this.emails.add(email); // To create a contact we need at least one emails.
-
     }
-
-
-
     public String getName() {
         return name;
     }
-
     public ArrayList<String> getEmail() {
         return emails;
     }
-
     public void addEmail(String emails){
         this.emails.add(emails);
     }
-
     public void removeEmail(String emails){
         if(this.emails.size() > 1)
             this.emails.remove(emails);
     }
-
-
     public void updateName(String name) {
         this.name = name;
     }
-
-
-
-
-
 }
