4e0ba4ddb8f988dd92db5ab5fefb019f30c487f4
ayman Algamal <ayman01287985950@gmail.com>
2023-12-22 19:07:51 +0200

    Modifying the addcontact command.

--- mail-server-backend/src/main/java/com/porvah/mailserver/models/ContactCommands/AddContactCommand.java
+++ mail-server-backend/src/main/java/com/porvah/mailserver/models/ContactCommands/AddContactCommand.java
@@ -1,6 +1,7 @@
 package com.porvah.mailserver.models.ContactCommands;
 
 import com.porvah.mailserver.models.Contact;
+import com.porvah.mailserver.models.UserBase;
 
 import java.util.ArrayList;
 import java.util.Map;
@@ -8,19 +9,24 @@ import java.util.Map;
 public class AddContactCommand implements ContactCommandIF {
 
     final private String name;
-    final private String email;
+    final private ArrayList<String> emails;
 
-    public AddContactCommand(String name, String email){
+    public AddContactCommand(String name, ArrayList<String> emails){
         this.name = name;
-        this.email = email;
+        for(String email : emails){
+            if(!UserBase.getInstance().containsUser(email)){
+                throw new IllegalArgumentException("Invalid email address");
+            }
+        }
+        this.emails = emails;
     }
 
 
     @Override
-    public boolean execute(Map<String, Contact> contacts) {
-        Contact contact = new Contact(name, email);
-        contacts.put(name, contact);
-        return true;
+    public int execute(Map<Integer, Contact> contacts) {
+        Contact contact = new Contact(name, emails);
+        contacts.put(contact.getContactId(), contact);
+        return contact.getContactId();
     }
 
 
